df980f795f5d39edddc8bd68c694e565e82e3f27
Kavyapriya <kavyapriya.govindarajan@cdw.com>
2023-03-17 12:04:44 +0530

    up

--- Task-2 Exception Handling/CreateIOException.java
+++
@@ -1,9 +0,0 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
-
-public class CreateIOException {
-	public static void main(String[] args){
-		FileReader file = new FileReader("test.txt");
-	}
-}
